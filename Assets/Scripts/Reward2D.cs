575743829a5ecc25de1f8f364c90e7f28fbe5ffe
Tim Ding <ding@ucdavis.edu>
2021-11-01 13:46:54 -0400

    Gaussian Noise to Velocity

--- Assets/Scripts/Reward2D.cs
+++ Assets/Scripts/Reward2D.cs
@@ -1308,7 +1308,14 @@ public class Reward2D : MonoBehaviour
                 //v12
                 velocity = velocities[11];
             }
-
+            System.Random randNoise = new System.Random();
+            double u1 = 1.0 - randNoise.NextDouble(); //uniform(0,1] random doubles
+            double u2 = 1.0 - randNoise.NextDouble();
+            double randStdNormal = Math.Sqrt(-2.0 * Math.Log(u1)) *
+                         Math.Sin(2.0 * Math.PI * u2); //random normal(0,1)
+                                                       //double randNormal =
+                                                       //mean + stdDev * randStdNormal; //random normal(mean,stdDev^2)
+            velocity = velocity + (float)randStdNormal;
             var direction = new Vector3();
             if (LRFB)
             {
