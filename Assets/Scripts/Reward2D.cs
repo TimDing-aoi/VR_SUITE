6a955435aa934a2f5ab3b2f058d537ab2a5fa7fb
Tim Ding <ding@ucdavis.edu>
2022-03-31 10:29:49 -0400

    fix

--- Assets/Scripts/Reward2D.cs
+++ Assets/Scripts/Reward2D.cs
@@ -3858,7 +3858,11 @@ public class Reward2D : MonoBehaviour
             {
                 for (int i = 0; i < temp[0]; i++)
                 {
+<<<<<<< HEAD
                     var line = string.Format("{0},{1},{2},{3},{4},{5},{6},{7},{8},{9},{10},{11},{12},{13},{14},{15},{16},{17},{18},{19},{20},{21},{22},{23},{24}",
+=======
+                    var line = string.Format("{0},{1},{2},{3},{4},{5},{6},{7},{8},{9},{10},{11},{12},{13},{14},{15},{16},{17},{18},{19},{20},{21},{22},{23}",
+>>>>>>> parent of ac7eede (some fixes)
                         n[i],
                         max_v[i],
                         max_w[i],
@@ -3883,7 +3887,12 @@ public class Reward2D : MonoBehaviour
                         ActionStart[i],
                         SelfReportStart[i],
                         FeedbackStart[i],
+<<<<<<< HEAD
                         CIScores[i]);
+=======
+                        FFnoiseList[i],
+                        GFFTrueDegree[i]);
+>>>>>>> parent of ac7eede (some fixes)
                     csvDisc.AppendLine(line);
                 }
             }
